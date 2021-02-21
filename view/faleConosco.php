b0e4e69f387a3c5fbfc7568658c8499faf27a88d
Daniel Millan <danieloliveiramillan@gmail.com>
2021-02-21 00:14:29 -0300

    tela fale conosco e home

---
+++ view/faleConosco.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Fale conosco</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="<?=root('css/bootstrap.min.css')?>" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="<?=root('css/mdb.min.css')?>" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="<?=root('css/style.min.css')?>" rel="stylesheet">
+  
+</head>
+
+<body>
+<!-- Navbar -->
+  <nav class="navbar fixed-top navbar-light white scrolling-navbar">
+    <div class="container">
+
+      <!-- Brand -->
+      <a class="navbar-brand waves-effect" href="<?=root()?>">
+        <strong class="blue-text"><?=$GLOBALS['APP_NAME']?></strong>
+      </a>
+
+
+		<ul class="navbar-nav nav-flex-icons float-right">
+          <li class="nav-item">
+            <a href = '<?= root('compra/carrinho')?>' class="nav-link border border-light rounded waves-effect">
+			&nbsp;
+              <i class="fas fa-shopping-cart"></i>
+              <span class="clearfix d-none d-sm-inline-block"> Carrinho </span>
+            &nbsp;
+			</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link border border-light rounded waves-effect"> 
+            &nbsp;
+			  <i class="fas fa-user"></i>
+			  <span class="clearfix d-none d-sm-inline-block"> Conta </span> 
+            &nbsp;
+			</a>
+          </li>
+        </ul>
+      <!-- Links -->
+    </div>
+  </nav>
+  <!-- Navbar -->
+
+<div class="mt-5 page-wrap d-flex flex-row align-items-center">
+
+    <div class="container mt-4 pt-4">
+	<div class="row justify-content-center">
+		<div class="col-12 col-md-8 col-lg-6 pb-5">
+
+
+                    <!--Form with header-->
+
+                    <form action="<?php root('cliente/contato')?>" method="post">
+                        <div class="card border-primary rounded-0">
+                            <div class="card-header p-0">
+                                <div class="bg-info text-white text-center py-2">
+                                    <h3><i class="fa fa-envelope"></i> Fale Conosco</h3>
+                                    <p class="m-0">Envie-nos uma mensagem</p>
+                                </div>
+                            </div>
+                            <div class="card-body p-3">
+
+                                <!--Body-->
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-user text-info"></i></div>
+                                        </div>
+                                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Seu nome" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-envelope text-info"></i></div>
+                                        </div>
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="Seu email" required>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-comment text-info"></i></div>
+                                        </div>
+                                        <textarea class="form-control" id="mensagem" name="mensagem" placeholder="Sua mensagem" required></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="text-center">
+                                    <input type="submit" value="Enviar" class="btn btn-info btn-block rounded-0 py-2">
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
+                    <!--Form with header-->
+
+
+                </div>
+	</div>
+</div>
+</div>
+<!--Footer-->
+  <footer class="page-footer text-center font-small mt-4 wow fadeIn">
+
+    <!--Call to action-->
+    <div class="pt-4">
+      <a class="btn btn-outline-white" href="<?=root('cliente/faleConosco')?>" role="button">
+	  Fale Conosco
+        <i class="fas fa-envelope ml-2"></i>
+      </a>
+      <a class="btn btn-outline-white" href="<?=root('sobre')?>" role="button">
+	  Sobre
+        <i class="fas fa-info-circle ml-2"></i>
+      </a>
+    </div>
+    <!--/.Call to action-->
+
+    <hr class="my-4">
+
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3">
+      © 2019 Copyright:
+      <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank"> MDBootstrap.com </a>
+	  <br>
+	  Daniel Oliveira Millan & Eduarda Dias Martins
+    </div>
+    <!--/.Copyright-->
+
+  </footer>
+  <!--/.Footer-->
+
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="<?=root('js/jquery-3.4.1.min.js')?>"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="<?=root('js/popper.min.js')?>"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="<?=root('js/bootstrap.min.js')?>"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="<?=root('js/mdb.min.js')?>"></script>
+  <!-- Initializations -->
+  <script type="text/javascript">
+    // Animations initialization
+    new WOW().init();
+  </script>
+</body>
+
+</html>
